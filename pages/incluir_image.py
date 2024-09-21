6d3fd8dec13595d0cb69f51bca9e003f3efa3625
Jhonyninho <157736198+Jhonyninho@users.noreply.github.com>
2024-09-21 10:20:50 -0300

    Delete pages/incluir_image.py

--- pages/incluir_image.py
+++
@@ -1,5 +0,0 @@
-#incluir depois
-image_path = '/Users/jhony/Desktop/CURSO ANALISTA/FTC - ANALISANDO DADOS COM PYTHON/REPOS'
-image = Image.open( image_path + 'logo.png' )
-st.sidebar.image ( image, width=120 )
-
